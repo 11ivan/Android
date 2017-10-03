2bb5a21e5008725a02bbd1374cf61dbfb4086054
11ivan <ivancastillocalle@gmail.com>
2017-10-03 10:56:19 +0200

    up

--- TextZoom/app/src/main/java/com/example/icastillo/textzoom/MainActivity.java
+++ TextZoom/app/src/main/java/com/example/icastillo/textzoom/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageButton btnPlus;
-    ImageButton btnMinus;
+   ImageButton btnPlus;
+   ImageButton btnMinus;
     EditText txtEdit;
     float fontSize;
     @Override
@@ -20,33 +20,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtEdit=(EditText) findViewById(R.id.texto);
         fontSize=txtEdit.getTextSize();
         txtEdit.setTextSize(fontSize);
-        //btnPlus=(ImageButton)findViewById(R.id.plus);
-        //btnMinus=(ImageButton) findViewById(R.id.minus);
-       //btnPlus.setOnClickListener(this);
-       // btnMinus.setOnClickListener(this);
-        findViewById(R.id.plus).setOnClickListener(this);
-        findViewById(R.id.minus).setOnClickListener(this);
+        btnPlus=(ImageButton)findViewById(R.id.plus);
+        btnMinus=(ImageButton) findViewById(R.id.minus);
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        //findViewById(R.id.plus).setOnClickListener(this);
+        //findViewById(R.id.minus).setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View view) {
-        //txtEdit=(EditText) findViewById(R.id.texto);
         switch(view.getId()){
             case R.id.plus:
-                //Toast.makeText(MainActivity.this, "Mas: "+txtEdit.getTextSize(),Toast.LENGTH_SHORT).show();
-                //txtEdit.setTextSize(TypedValue.COMPLEX_UNIT_DIP, txtEdit.getTextSize()+1);
-                //fontSize++;
                 txtEdit.setTextSize(++fontSize);
-
                 break;
             case R.id.minus:
-                    //Toast.makeText(MainActivity.this, "Menos: "+txtEdit.getTextSize(),Toast.LENGTH_SHORT).show();
-                    //txtEdit.setTextSize(TypedValue.COMPLEX_UNIT_DIP, txtEdit.getTextSize()-1);
-                //fontSize--;
                 txtEdit.setTextSize(--fontSize);
                 break;
-
             default:
                 break;
         }
