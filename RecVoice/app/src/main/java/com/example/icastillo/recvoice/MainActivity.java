fdaf5268c5a41135897721630804b8ed030cd503
Unknown <07.qweasd123@gmail.com>
2017-10-16 12:45:23 +0200

    ListView

--- RecVoice/app/src/main/java/com/example/icastillo/recvoice/MainActivity.java
+++ RecVoice/app/src/main/java/com/example/icastillo/recvoice/MainActivity.java
@@ -66,36 +66,25 @@ public class MainActivity extends Activity {
 
 
 /*
-* int permissionCheck = ContextCompat.checkSelfPermission(thisActivity,
-        Manifest.permission.WRITE_CALENDAR);
-*
-*
-*
-*
-* // Here, thisActivity is the current activity
-if (ContextCompat.checkSelfPermission(thisActivity,
-                Manifest.permission.READ_CONTACTS)
-        != PackageManager.PERMISSION_GRANTED) {
-
+int permissionCheck = ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.WRITE_CALENDAR);
+// Here, thisActivity is the current activity
+if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED) {
     // Should we show an explanation?
-    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
-            Manifest.permission.READ_CONTACTS)) {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity, Manifest.permission.READ_CONTACTS)) {
 
         // Show an explanation to the user *asynchronously* -- don't block
         // this thread waiting for the user's response! After the user
         // sees the explanation, try again to request the permission.
 
-    } else {
+} else {
 
         // No explanation needed, we can request the permission.
 
-        ActivityCompat.requestPermissions(thisActivity,
-                new String[]{Manifest.permission.READ_CONTACTS},
-                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
         // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
         // app-defined int constant. The callback method gets the
         // result of the request.
     }
 }
-* */
+*/
