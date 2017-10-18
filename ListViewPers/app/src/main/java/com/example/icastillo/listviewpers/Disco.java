18696ec1a552784110a51cd28e87e6885d3ba5cd
Unknown <07.qweasd123@gmail.com>
2017-10-18 14:31:13 +0200

    ListViewPers

--- ListViewPers/app/src/main/java/com/example/icastillo/listviewpers/Disco.java
+++ ListViewPers/app/src/main/java/com/example/icastillo/listviewpers/Disco.java
@@ -2,7 +2,10 @@ package com.example.icastillo.listviewpers;
 
 import android.widget.ImageView;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
 
 /**
  * Created by icastillo on 17/10/17.
@@ -12,29 +15,86 @@ import java.util.Date;
    Propiedades Basicas:
         Nombre:
         Artista:
+        Imagen:
 
    Propiedades Derivadas:
 
 
    Propiedades Agregadas:
         Date
-        ImageView
+
 
  */
 
 public class Disco {
-    private String Nombre;
-    private String Artista;
-    private Date fecha;
-    private ImageView imagen;
+    private String nombre;
+    private String artista;
+    private GregorianCalendar fecha;
+    private String imagen;
 
-    public Disco(){
-
-        Nombre=" ";
-        Artista=" ";
-        fecha=new Date();
-        imagen=new ImageView();
+    //Constructores
+    public Disco() {
+        nombre = " ";
+        artista = " ";
+        fecha = new GregorianCalendar();
+        imagen = " ";
     }
 
+    public Disco(String nombre, String artista, GregorianCalendar fecha, String imagen) {
+        this.nombre = nombre;
+        this.artista = artista;
+        this.fecha = (GregorianCalendar) fecha.clone();
+        this.imagen = imagen;
+    }
+
+
+    //Consultores
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getArtista() {
+        return this.artista;
+    }
+
+
+    public GregorianCalendar getFecha() {
+        return this.fecha;
+    }
+
+    public String getImagen() {
+        return this.imagen;
+    }
+
+    //Modificadores
+    public void setNombre(String nombre) {
+        if (nombre != null && nombre.replaceAll(" ", "").length() > 0) {
+            this.nombre = nombre;
+        }
+    }
+
+    public void setArtista(String artista) {
+        if (artista != null && artista.replaceAll(" ", "").length() > 0) {
+            this.artista = artista;
+        }
+    }
+
+    public void setFecha(GregorianCalendar fecha) {
+        //if(fecha!=null && fecha.before(GregorianCalendar.getInstance())){
+            this.fecha = fecha;
+        //}
+    }
+
+    public void setImagen(String imagen) {
+        if (imagen != null && imagen.replaceAll(" ", "").length() > 0) {
+            this.imagen = imagen;
+        }
+    }
+
+    public String toString(){
+        String cadena;
+            cadena=this.nombre+", "+this.artista+", "+fecha.get(Calendar.DAY_OF_MONTH)+"/"+fecha.get(Calendar.MONTH)+"/"+fecha.get(Calendar.YEAR);
+        return cadena;
+    }
 
 }
--- ListViewPers/app/src/main/java/com/example/icastillo/listviewpers/MainActivity.java
+++ ListViewPers/app/src/main/java/com/example/icastillo/listviewpers/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.icastillo.listviewpers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    IconicAdapter <Disco> myadapter;
+    TextView textView;
+    Disco[] arrayDiscos={new Disco("Blakanguai", "Varios artistas", new GregorianCalendar(2016, 10, 5),String.valueOf(R.drawable.bw)),
+                        new Disco("Hay", "O no hay", new GregorianCalendar(2015, 1, 15),String.valueOf(R.drawable.bw)),
+                        new Disco("Pffff", "Quien tu quieras", new GregorianCalendar(2017, 5, 25), String.valueOf(R.drawable.bw))};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView) findViewById(R.id.txtView);
+        listView=(ListView) findViewById(R.id.list);
+        myadapter=new IconicAdapter<Disco>(this, R.layout.stylelist, R.id.txtView, arrayDiscos);
+        listView.setAdapter(myadapter);
+
     }
+
+
+    class IconicAdapter<T> extends ArrayAdapter<T> {
+        IconicAdapter(Context c, int resourceId, int textId, T[] objects) {
+            super(c, resourceId, textId, objects);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = super.getView(position, convertView, parent);
+            ImageView imagen = (ImageView) row.findViewById(R.id.imagen);
+
+            imagen.setImageResource(Integer.parseInt(arrayDiscos[position].getImagen()));
+
+            /*if (items[position].equals("calendario")) {
+                icon.setImageResource(R.drawable.calendar48x48);
+            }
+            else if (items[position].equals("GPS")){
+                icon.setImageResource(R.drawable.compass48x48);
+            }
+            else if (items[position].equals("alarma")){
+                icon.setImageResource(R.drawable.alarm48x48);
+            }
+            else{
+                icon.setImageResource(R.drawable.delete);
+            }*/
+
+            return (row);
+        }
+
+    }//fin clase iconicAdapter
+
+
 }
