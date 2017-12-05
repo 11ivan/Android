33fd10aa88469a55a40638c74cd42604b04e1d99
Unknown <07.qweasd123@gmail.com>
2017-12-05 11:49:29 +0100

    up

--- PirdraPapelTijeras/app/src/main/java/com/example/icastillo/pirdrapapeltijeras/JuegoActivity.java
+++ PirdraPapelTijeras/app/src/main/java/com/example/icastillo/pirdrapapeltijeras/JuegoActivity.java
@@ -1,11 +1,13 @@
 package com.example.icastillo.pirdrapapeltijeras;
 
 import android.media.Image;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -39,27 +41,69 @@ public class JuegoActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        haPulsado=!haPulsado;
         if(!haPulsado) {
+            haPulsado=true;
             switch (v.getId()) {
-
                 case R.id.btnPiedra:
-                    imagenMiEleccion.setImageResource(arrayImagenes[0]);
-                    posLevantadaUsuario=0;
+                    posLevantadaUsuario = 0;
                     break;
 
                 case R.id.btnPapel:
-                    imagenMiEleccion.setImageResource(arrayImagenes[1]);
-                    posLevantadaUsuario=1;
+                    posLevantadaUsuario = 1;
                     break;
 
                 case R.id.btnTijeras:
-                    imagenMiEleccion.setImageResource(arrayImagenes[2]);
-                    posLevantadaUsuario=2;
+                    posLevantadaUsuario = 2;
                     break;
             }
+            changeImageUsuario(posLevantadaUsuario);
+            generaImagenMaquina();
+            mensajeGanador(compruebaGanador());
+            imagenesPorDefecto();
         }
     }
+
+
+    /*
+    * Proposito: Según el ganador muestra mensaje de ganador, empate o perdedor
+    * Precondiciones: ha habido algún ganador o empate
+    * Entradas: un entero qe será 1 si ha ganado el usuario 0 si hay empate y -1 si gana la maquina
+    * Salidas: No hay
+    * Postcondiciones: Se ha lanzado un mensaje según el ganador
+    * */
+    public void mensajeGanador(int resultado){
+        String mensaje="";
+        switch (resultado){
+            case 0:
+                    mensaje="Hay empate";
+                break;
+
+            case 1:
+                    mensaje="Ha ganado el jugador";
+                break;
+
+            case -1:
+                    mensaje="Ha ganado la máquina";
+                break;
+        }
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+
+    /*
+    * Proposito: Cambia la imagen para el usuario según la que haya elegido
+    * Precondiciones: El Usuario ha pulsado algún botón
+    * Entradas: No hay
+    * Salidas: No hay
+    * Postcondiciones: Se ha cambiado la imagen por la elegida por el usuario
+    * */
+    public void changeImageUsuario(int posicion){
+        if(haPulsado) {
+            //haPulsado=true;
+            imagenMiEleccion.setImageResource(arrayImagenes[posicion]);
+        }
+    }
+
 
     /*
     * Proposito: Genera una imagen al azar entre piedra, papel o tijeras
@@ -68,39 +112,49 @@ public class JuegoActivity extends AppCompatActivity implements View.OnClickList
     * Salidas: No hay
     * Postcondiciones: Se ha cambiado la imagen por la generada de forma aleatoria
     * */
-    public void generaImagen(){
+    public void generaImagenMaquina(){
         Random aleatorio=new Random();
         int generado=aleatorio.nextInt(3);
-        switch (generado){
-            case 0:
-                    imagenEleccionMaquina.setImageResource(arrayImagenes[0]);
-                    posLevantadaMaquina=0;
-                break;
+        if(haPulsado) {
+            switch (generado) {
+                case 0:
+                    posLevantadaMaquina = 0;
+                    break;
 
-            case 1:
-                    imagenEleccionMaquina.setImageResource(arrayImagenes[1]);
-                    posLevantadaMaquina=1;
-                break;
+                case 1:
+                    posLevantadaMaquina = 1;
+                    break;
 
-            case 2:
-                    imagenEleccionMaquina.setImageResource(arrayImagenes[2]);
-                    posLevantadaMaquina=2;
-                break;
+                case 2:
+                    posLevantadaMaquina = 2;
+                    break;
+            }
+            imagenEleccionMaquina.setImageResource(arrayImagenes[posLevantadaMaquina]);
         }
     }
 
-/*
+
+        /*
+    * Proposito: Pone la imagen por defecto en las elecciones
+    * Precondiciones: hay dos imagenes elegidas
+    * Entradas: no hay
+    * Salidas: No hay
+    * Postcondiciones: Se ha lanzado un mensaje según el ganador
+    * */
+    public void imagenesPorDefecto(){
         Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                ImagescartasLevantadas[0].setImageResource(R.drawable.reverso);
-                ImagescartasLevantadas[1].setImageResource(R.drawable.reverso);
-                desmarcaCartasLevantada(posicionLevantadas);
+                imagenEleccionMaquina.setImageResource(R.drawable.interrogante);
+                imagenMiEleccion.setImageResource(R.drawable.interrogante);
+                haPulsado=false;
             }
         },1500);
-*/
+        //imagenEleccionMaquina.setImageResource(R.drawable.interrogante);
+        //imagenMiEleccion.setImageResource(R.drawable.interrogante);
+    }
 
 
     /*
@@ -113,10 +167,18 @@ public class JuegoActivity extends AppCompatActivity implements View.OnClickList
     public int compruebaGanador(){
         int ganador=0;
 
-        if(posLevantadaMaquina){
-
+        if(haPulsado) {
+            //El usuario gana
+            if (posLevantadaMaquina == 0 && posLevantadaUsuario == 1 || posLevantadaMaquina == 1 && posLevantadaUsuario == 2 || posLevantadaMaquina == 2 && posLevantadaUsuario == 0) {
+                ganador = 1;
+                //La maquina gana
+            } else if (posLevantadaUsuario == 0 && posLevantadaMaquina == 1 || posLevantadaUsuario == 1 && posLevantadaMaquina == 2 || posLevantadaUsuario == 2 && posLevantadaMaquina == 0) {
+                ganador = -1;
+            } else {
+                ganador = 0;
+            }
+            //haPulsado=false;
         }
-
         return ganador;
     }
 
