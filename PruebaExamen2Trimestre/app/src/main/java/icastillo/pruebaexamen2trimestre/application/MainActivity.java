900edda2158cf6e4a09b5bb2168e6a30082e132c
Unknown <07.qweasd123@gmail.com>
2018-02-20 12:45:05 +0100

    up

---
+++ PruebaExamen2Trimestre/app/src/main/java/icastillo/pruebaexamen2trimestre/application/MainActivity.java
@@ -0,0 +1,21 @@
+package icastillo.pruebaexamen2trimestre.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import icastillo.pruebaexamen2trimestre.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+}
---
+++ PruebaExamen2Trimestre/app/src/main/java/icastillo/pruebaexamen2trimestre/room/repositorios/RepositorioPersonas.java
@@ -0,0 +1,11 @@
+package icastillo.pruebaexamen2trimestre.room.repositorios;
+
+/**
+ * Created by icastillo on 20/02/18.
+ */
+
+public class RepositorioPersonas {
+
+
+
+}
